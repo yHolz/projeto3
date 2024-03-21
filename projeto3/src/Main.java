67c7f9cf0c0a2057fb70ff5bda57402656f4c6cd
yHolz <matheusholz2001@hotmail.com>
2024-03-20 21:15:13 -0300

    Pagina de Cadastro

--- projeto3/src/Main.java
+++ projeto3/src/Main.java
@@ -3,5 +3,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Pagina de cadastro");
+        System.out.println("Usuario:teste");
+        System.out.println("senha:*****");
+        System.out.println("CPF:12345678900");
+        System.out.println("E-mail:abc@abc.com");
+        System.out.println("(Cadastrar)");
+        
         }
     }
